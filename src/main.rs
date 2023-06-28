1f411e06b525716176f8f2b8af8a79e19fc8c019
YUK1 <116713322+YUK1-0000@users.noreply.github.com>
2023-06-28 09:51:09 +0000

    aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,20 @@
+#![cfg_attr(not(test), windows_subsystem = "windows")]
+#![cfg_attr(test, windows_subsystem = "console")]
+
+use libui::controls::*;
+use libui::prelude::*;
+
 fn main() {
-    println!("Hello, world!");
+    let ui = UI::init()
+        .expect("Couldn't initialize UI library");
+    
+    let mut win = Window::new(&ui, "Example", 300, 200, 
+        WindowType::NoMenubar);
+    let layout = VerticalBox::new();
+
+    // add controls to your layout here
+
+    win.set_child(layout);
+    win.show();
+    ui.main();
 }
