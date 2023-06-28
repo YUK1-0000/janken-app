bc593eb54bfeb4f89ac9f96a6e3f012876c3e392
YUK1-0000 <tanaka543taro@gmail.com>
2023-06-28 20:12:52 +0900

    button

--- src/main.rs
+++ src/main.rs
@@ -1,18 +1,35 @@
 #![cfg_attr(not(test), windows_subsystem = "windows")]
 #![cfg_attr(test, windows_subsystem = "console")]
 
-use libui::controls::*;
-use libui::prelude::*;
+use libui::{controls::*, prelude::*};
 
 fn main() {
     let ui = UI::init()
         .expect("Couldn't initialize UI library");
     
-    let mut win = Window::new(&ui, "Example", 300, 200, 
+    let mut win = Window::new(&ui, "じゃんけん", 300, 200, 
         WindowType::NoMenubar);
-    let layout = VerticalBox::new();
+    let mut layout = HorizontalBox::new();
 
-    // add controls to your layout here
+
+    let mut button_a = Button::new("A");
+    let mut button_b = Button::new("B");
+
+    let mut cloned_button_b = button_b.clone();
+    button_a.on_clicked(move |button_a| {
+        button_a.hide();
+        cloned_button_b.show();
+    });
+
+    let mut cloned_button_a = button_a.clone();
+    button_b.on_clicked(move |button_b| {
+        button_b.hide();
+        cloned_button_a.show();
+    });
+
+    layout.append(button_a, LayoutStrategy::Compact);
+    layout.append(button_b, LayoutStrategy::Compact);
+    
 
     win.set_child(layout);
     win.show();
