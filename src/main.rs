cd8f421c6008bcbc6c042023b0bcfde4ec36a29b
YUK1-0000 <tanaka543taro@gmail.com>
2023-07-15 13:00:46 +0900

    勝敗数、勝率を表示

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,8 @@
 #![cfg_attr(not(test), windows_subsystem = "windows")]
 #![cfg_attr(test, windows_subsystem = "console")]
 
+use std::{ cell::RefCell, rc::Rc };
+
 use libui::{controls::*, prelude::*};
 use rand::Rng;
 
@@ -9,14 +11,14 @@ const RESULT_TYPES: [&str;3] = ["勝ち", "負け", "あいこ"];
 
 
 struct Janken {
-    win: usize,
-    lose: usize
+    win: f32,
+    lose: f32
 }
 
 
 impl Janken {
     fn new() -> Self {
-        Self { win: 0, lose: 0 }
+        Self { win: 0., lose: 0. }
     }
 
     fn rand_idx(&self) -> usize {
@@ -30,11 +32,11 @@ impl Janken {
 
     fn result(&mut self, player_idx: usize, cpu_idx: usize) -> &'static str {
         if HAND_TYPES[cpu_idx + 1] == HAND_TYPES[player_idx] {
-            self.win += 1;
+            self.win += 1.;
             RESULT_TYPES[0]
         }
         else if HAND_TYPES[player_idx + 1] == HAND_TYPES[cpu_idx] {
-            self.lose += 1;
+            self.lose += 1.;
             RESULT_TYPES[1]
         }
         else {
@@ -42,18 +44,41 @@ impl Janken {
         }     
     }
 
-    fn update(&mut self) -> (usize, &'static str) {
+    fn on_update(&mut self, i: usize) -> (usize, &'static str) {
         let j = self.rand_idx();
-        (j, self.result(0, j))
+        (j, self.result(i, j))
     }
 }
 
 
+#[derive(Clone)]
 struct Labels {
     player: Label,
     cpu: Label,
     result: Label,
-    count: Label
+    count: Label, 
+    win_rate: Label
+}
+
+
+impl Labels {
+    fn new() -> Self {
+        Self {
+            player: Label::new("あなた："), 
+            cpu: Label::new("CPU："), 
+            result: Label::new(""), 
+            count: Label::new("0勝0敗"), 
+            win_rate: Label::new("")
+        }
+    }
+
+    fn update(&mut self, i: usize, j: usize, result: &str, win: f32, lose: f32) {
+        self.player.set_text(&format!("あなた：{}", HAND_TYPES[i]));
+        self.cpu.set_text(&format!("CPU：{}", HAND_TYPES[j]));
+        self.result.set_text(result);
+        self.count.set_text(&format!("{}勝{}敗", win, lose));
+        self.win_rate.set_text(&format!("勝率{}", win / (win + lose)));
+    }
 }
 
 
@@ -61,7 +86,8 @@ fn main() {
     let ui = UI::init().expect("Couldn't initialize UI library");
 
     let mut window = Window::new(
-        &ui, "じゃんけん", 300, 100, WindowType::NoMenubar
+        &ui, "じゃんけん", 300, 100,
+        WindowType::NoMenubar
     );
 
     let mut layout = VerticalBox::new();
@@ -72,50 +98,33 @@ fn main() {
     let mut paper_btn = Button::new("パー");
     let mut scissors_btn = Button::new("チョキ");
 
-    let player_lbl = Label::new("あなた:");
-    let mut player_lbl_cln_1 = player_lbl.clone();
-    let mut player_lbl_cln_2 = player_lbl.clone();
-    let mut player_lbl_cln_3 = player_lbl.clone();
+    let janken = Rc::new(RefCell::new(Janken::new()));
 
-    let cpu_lbl = Label::new("CPU:");
-    let mut cpu_lbl_cln_1 = cpu_lbl.clone();
-    let mut cpu_lbl_cln_2 = cpu_lbl.clone();
-    let mut cpu_lbl_cln_3 = cpu_lbl.clone();
+    let janken_cln_1 = Rc::clone(&janken);
+    let janken_cln_2 = Rc::clone(&janken);
+    let janken_cln_3 = Rc::clone(&janken);
 
-    let result_lbl = Label::new("");
-    let mut result_lbl_cln_1 = result_lbl.clone();
-    let mut result_lbl_cln_2 = result_lbl.clone();
-    let mut result_lbl_cln_3 = result_lbl.clone();
+    let labels = Labels::new();
 
-
-    let janken = Janken::new();
+    let mut labels_cln_1 = labels.clone();
+    let mut labels_cln_2 = labels.clone();
+    let mut labels_cln_3 = labels.clone();
 
     rock_btn.on_clicked(move |_rock_btn| {
-        let (j, result) = janken.update();
-        player_lbl_cln_1.set_text(&format!("{} {}", "あなた:", HAND_TYPES[0]));
-        cpu_lbl_cln_1.set_text(&format!("{} {}", "CPU:", HAND_TYPES[j]));
-        result_lbl_cln_1.set_text(result)
+        update_labels(&janken_cln_1, &mut labels_cln_1, 0);
     });
 
     paper_btn.on_clicked(move |_paper_btn| {
-        let i = 1;
-        let j = janken.rand_idx();
-        player_lbl_cln_2.set_text(&format!("{} {}", "あなた:", HAND_TYPES[i]));
-        cpu_lbl_cln_2.set_text(&format!("{} {}", "CPU:", HAND_TYPES[j]));
-        result_lbl_cln_2.set_text(janken.result(i, j))
+        update_labels(&janken_cln_2, &mut labels_cln_2, 1);
     });
 
     scissors_btn.on_clicked(move |_scissors_btn| {
-        let i = 2;
-        let j = janken.rand_idx();
-        player_lbl_cln_3.set_text(&format!("{} {}", "あなた:", HAND_TYPES[i]));
-        cpu_lbl_cln_3.set_text(&format!("{} {}", "CPU:", HAND_TYPES[j]));
-        result_lbl_cln_3.set_text(janken.result(i, j))
+        update_labels(&janken_cln_3, &mut labels_cln_3, 2);
     });
     
 
-    for control in [player_lbl, cpu_lbl, result_lbl] {
-        messages.append(control, LayoutStrategy::Stretchy);
+    for child in [labels.player, labels.cpu, labels.result, labels.count, labels.win_rate] {
+        messages.append(child, LayoutStrategy::Stretchy);
     };
 
     for child in [rock_btn, scissors_btn, paper_btn] {
@@ -130,10 +139,7 @@ fn main() {
     ui.main();
 }
 
-fn update_lbl(core: &mut Janken, labels: &mut Labels) {
-    let (j, result) = core.update();
-    labels.player.set_text(&format!("{} {}", "あなた:", HAND_TYPES[0]));
-    labels.cpu.set_text(&format!("{} {}", "CPU:", HAND_TYPES[j]));
-    labels.result.set_text(result)
-    
+fn update_labels(core: &Rc<RefCell<Janken>>, labels: &mut Labels, i: usize) {
+    let (j, result) = core.borrow_mut().on_update(i);
+    labels.update(i, j, result, core.borrow().win, core.borrow().lose);
 }
